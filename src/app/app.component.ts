fbb220e3db53d8d495b58f275a7d817b1077e087
LucindaDev <lucindadsanchez@gmail.com>
2024-11-13 11:55:49 -0700

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GamelistComponent } from "./gamelist/gamelist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GamelistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
