43ce4c7b571478f8f1341fa763a2b2ccf2389734
LucindaDev <lucindadsanchez@gmail.com>
2024-11-14 10:43:42 -0700

    se agrega filtro de busqueda por titulo

--- src/app/gamelist/gamelist.component.ts
+++ src/app/gamelist/gamelist.component.ts
@@ -3,24 +3,67 @@ import { GameServiceService } from '../services/game-service.service';
 import { Game } from '../interfaces/Game';
 import { HttpClientModule } from '@angular/common/http';
 import { NgFor } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gamelist',
   standalone: true,
-  imports: [HttpClientModule, NgFor],
+  imports: [HttpClientModule, NgFor, ReactiveFormsModule],
   providers: [GameServiceService],
   templateUrl: './gamelist.component.html',
   styleUrl: './gamelist.component.css'
 })
 export class GamelistComponent {
   games: Game[] = [];
+  filteredGames: Game[] = [];
+  searchForm: FormGroup;
 
-  constructor(private gameService: GameServiceService) { }
+  constructor(
+    private gameService: GameServiceService,
+    private formBuilder: FormBuilder
+  ) {
+    this.searchForm = this.formBuilder.group({
+      searchTitle: ['']
+    });
+  }
 
   ngOnInit() {
-    this.gameService.getGames().subscribe((data) => {
-      this.games = data;
-    });
+    this.loadAllGames();
+    this.setUpSearch();
+  }
+
+  loadAllGames() {
+    this.gameService.getGames().subscribe({
+      next: (games) => {
+        this.games = games;
+        this.filteredGames = games;
+      },
+      error: (error) => {
+        console.log('Error fetching games');
+      }
+    })
+  }
+
+  setUpSearch(): void {
+    this.searchForm.get('searchTitle')?.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe(searchTerm => {
+        this.filterByTitle(searchTerm);
+      });
+  }
+
+  filterByTitle(title: string): void {
+    if (!title) {
+      this.filteredGames = this.games;
+    } else {
+      this.filteredGames = this.games.filter(game =>
+        game.title.toLowerCase().includes(title.toLowerCase())
+      );
+    }
   }
 
   onchangePlatform(event: any) {
